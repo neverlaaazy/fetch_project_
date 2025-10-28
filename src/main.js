1d47ebc98d6600b0717534ff836dc2b34a96e8ae
neverlaaazy <cheserega174@gmail.com>
2025-10-28 11:15:10 +0500

    addPosts

--- src/main.js
+++ src/main.js
@@ -2,39 +2,39 @@ import './style.css'
 
 const container = document.getElementById("app");
 
-fetch('http://localhost:3000/posts?userId=10')
- .then((response) => {
- 	if (response.ok) return response;
- 	throw new Error(`${response.status} — ${response.statusText}`);
- })
- .then((response) => response.json())
- .then((posts) => {
-    posts.forEach((post) => {
-      let card = document.createElement('div');
-      card.innerHTML = `<h2>${post.title}</h2>`;
-      // card.innerHTML = `<h2>${post.body}</h2>`;
-      card.classList.add('card');
-      container.append(card);
-    });
- })
- .catch((error) => console.log(error));
+// fetch('http://localhost:3000/posts?userId=1')
+//  .then((response) => {
+//  	if (response.ok) return response;
+//  	throw new Error(`${response.status} — ${response.statusText}`);
+//  })
+//  .then((response) => response.json())
+//  .then((posts) => {
+//     posts.forEach((post) => {
+//       let card = document.createElement('div');
+//       card.innerHTML = `<h2>${post.title}</h2>`;
+//       // card.innerHTML = `<h2>${post.body}</h2>`;
+//       card.classList.add('card');
+//       container.append(card);
+//     });
+//  })
+//  .catch((error) => console.log(error));
 
 
 const article = document.querySelector(".article");
-async function myFetch(){
-  let response = await fetch('http://localhost:3000/posts/1');
-  let post = await response.json();
-  let card = document.createElement('div');
-  card.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`;
-  card.classList.add('card');
-  article.append(card);
- }
+// async function myFetch(){
+//   let response = await fetch('http://localhost:3000/posts/1');
+//   let post = await response.json();
+//   let card = document.createElement('div');
+//   card.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`;
+//   card.classList.add('card');
+//   article.append(card);
+//  }
 
-myFetch();
+// myFetch();
 
 const userI = document.querySelector(".usersInfo");
 
-fetch('http://localhost:3000/users?userId=10')
+fetch('http://localhost:3000/users')
  .then((response) => {
  	if (response.ok) return response;
  	throw new Error(`${response.status} — ${response.statusText}`);
@@ -43,10 +43,50 @@ fetch('http://localhost:3000/users?userId=10')
  .then((users) => {
     users.forEach((user) => {
       let UserCard = document.createElement('div');
-      UserCard.innerHTML = `<a>${user.name}</a>`;
-      // card.innerHTML = `<h2>${post.body}</h2>`;
+      UserCard.innerHTML = `<a>${user.id}) ${user.name}</a>`;
       UserCard.classList.add('UserCard');
+      UserCard.dataset.userID = user.id;
+      UserCard.addEventListener('click', () => {
+            fetchPostsByUser(user.id);
+        });
       userI.append(UserCard);
     });
  })
  .catch((error) => console.log(error));
+
+ function fetchPostsByUser(userId) {
+    fetch(`http://localhost:3000/posts?userId=${userId}`)
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error(`${response.status} — ${response.statusText}`);
+        })
+        .then(posts => {
+            container.innerHTML = '';
+            posts.forEach(post => {
+                let card = document.createElement('div');
+                
+                card.innerHTML = `<h2>${post.id}- ${post.title}</h2><p>${post.body}</p>`;
+                card.classList.add('card');
+                container.append(card);
+            });
+        })
+        .catch(error => console.error(error));
+}
+
+
+// fetch('http://localhost:3000/posts?userId=1')
+//  .then((response) => {
+//  	if (response.ok) return response;
+//  	throw new Error(`${response.status} — ${response.statusText}`);
+//  })
+//  .then((response) => response.json())
+//  .then((posts) => {
+//     posts.forEach((post) => {
+//       let card = document.createElement('div');
+//       card.innerHTML = `<h2>${post.title}</h2>`;
+//       // card.innerHTML = `<h2>${post.body}</h2>`;
+//       card.classList.add('card');
+//       container.append(card);
+//     });
+//  })
+//  .catch((error) => console.log(error));
